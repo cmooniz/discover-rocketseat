74f271e153755baedfccff528a7e227e56f2dc42
cmooniz <cmuniz.27@outlook.com>
2023-08-24 21:15:45 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img");
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./media/avatar-light.png");
+  } else {
+    img.setAttribute("src", "./media/avatar.png");
+  }
+}
